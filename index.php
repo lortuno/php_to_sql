6ae8eafdc4757f4fa5a2cdebe19a6ceca51ae32c
Laura Ortuño <laura.ortuno@trialpanel.com>
2019-06-22 00:11:41 +0200

    Modifies template index

--- index.php
+++ index.php
@@ -10,19 +10,23 @@
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" >
 </head>
-<article>
-    <h1>Get SQL Script to execute</h1>
-    <section>
+<article class="container-fluid">
+    <h1 class="text-info text-md-center">Get SQL Script to execute</h1>
+    <section class="container">
         <h2>Choose your options</h2>
         <div class="row">
-            <div class="col-xs-12 col-md-6">
-                <p>Environment DEV</p>
-                <a href="/uploads/products_shop.php?environment=dev" class="btn btn-success"></a>
-            </div>
-            <div class="col-xs-12 col-md-6">
-                <p>Environment PROD</p>
-                <a href="/uploads/products_shop.php?environment=prod" class="btn btn-info"></a>
-            </div>
+			<div class="col-xs-12">
+				<p>Environment </p>
+				<div class="row">
+					<div class="col-xs-12 col-md-6">
+						<a href="/uploads/products_shop.php?environment=dev" class="btn btn-success">DEV</a>
+					</div>
+					<div class="col-xs-12 col-md-6">
+						<a href="/uploads/products_shop.php?environment=prod" class="btn btn-info">PROD</a>
+					</div>
+				</div>
+			</div>
+
         </div>
     </section>
 </article>
