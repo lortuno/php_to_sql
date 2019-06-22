c98ef4538ad953e5685d1367e2b11ee7d4cd7dc0
Laura Ortuño <laura.ortuno@trialpanel.com>
2019-06-22 12:26:02 +0200

    Adds explanation

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Generates a script to launch in DB
 
 # Introduction
 This data is only for testing
+This projects aims to generate SQL to execute in the database, processing php arrays with the data for an adapted database structure, allowing to save time while the database manager is created (which is the real way to change data in a database)
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include_once ('uploads/sections/section_head.php')?>
 <article class="container">
 	<h1 class="text-info text-md-left">Get SQL Script to execute</h1>
-	<p>The main purpose of this page is loading a file with the variables to process, send the to the file that has the SQL structure and generate
+	<p>The main purpose of this page is loading a file with the variables to process, send them to the file that has the SQL structure and generate
 	a SQL script to upload or modify the database in a simple way, avoiding mechanical tasks.</p>
 	<p>Of course, the best way to do this is having a database manager, but this script allows to modify data in a quick manner.</p>
 	<section>
@@ -10,13 +10,14 @@
 		<form action="/uploads/products_shop.php" method="get">
 			<div class="form-row">
 				<div class="form-group col-md-6">
-					<label for="file1">Introduce File name with variables to load</label>
+					<label for="file1"><strong>Introduce File name with variables to load</strong></label>
 					<input type="text" name="filename" class="form-control" id="file1" placeholder="file">
 				</div>
 			</div>
 			<div class="form-row">
 				<div class="form-group col-xs-12">
-					<p>Environment</p>
+					<p><strong>Environment</strong></p>
+					<p>There might be changes between databases</p>
 				</div>
 			</div>
 			<div class="form-row">
